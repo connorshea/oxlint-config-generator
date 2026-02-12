479f0aab351f5e36e185023bcac542a0af4bc560
Connor Shea <connor.james.shea@gmail.com>
2026-02-11 19:59:05 -0700

    Fmt.

--- src/shared/js-plugin-configs.ts
+++ src/shared/js-plugin-configs.ts
@@ -11,15 +11,35 @@ export interface JSPluginConfig {
 // When adding a new JS plugin, add it here and update JSPluginName/RulePrefix in src/types/index.ts.
 // NOTE: rulePrefix should always be `@npmscope` if the package is `@npmscope/eslint-plugin`.
 export const jsPluginConfigs: JSPluginConfig[] = [
-  { name: "playwright", packageName: "eslint-plugin-playwright", rulePrefix: "playwright", label: "Playwright" },
-  { name: "stylistic", packageName: "@stylistic/eslint-plugin", rulePrefix: "@stylistic", label: "Stylistic" },
-  { name: "storybook", packageName: "eslint-plugin-storybook", rulePrefix: "storybook", label: "Storybook" },
+  {
+    name: "playwright",
+    packageName: "eslint-plugin-playwright",
+    rulePrefix: "playwright",
+    label: "Playwright",
+  },
+  {
+    name: "stylistic",
+    packageName: "@stylistic/eslint-plugin",
+    rulePrefix: "@stylistic",
+    label: "Stylistic",
+  },
+  {
+    name: "storybook",
+    packageName: "eslint-plugin-storybook",
+    rulePrefix: "storybook",
+    label: "Storybook",
+  },
   {
     name: "testing-library",
     packageName: "eslint-plugin-testing-library",
     rulePrefix: "testing-library",
     label: "Testing Library",
   },
-  { name: "cypress", packageName: "eslint-plugin-cypress", rulePrefix: "cypress", label: "Cypress" },
+  {
+    name: "cypress",
+    packageName: "eslint-plugin-cypress",
+    rulePrefix: "cypress",
+    label: "Cypress",
+  },
   { name: "e18e", packageName: "@e18e/eslint-plugin", rulePrefix: "@e18e", label: "e18e" },
 ];
